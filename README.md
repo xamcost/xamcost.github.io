37e79f80f98c7ff8df5223372bede68383d81994
Maxime Costalonga <maxime.costalonga.external@eviden.com>
2025-02-16 20:09:27 +0000

    maint: remove tailwindcss

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Xamcost Personal Website
 
 This repository contains the source code for my personal website. It uses [Astro](https://astro.build) as the 
-static site generator and [Tailwind CSS](https://tailwindcss.com) for styling.
+static site generator.
--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config'
 
-import tailwindcss from '@tailwindcss/vite'
-
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://astronaut.github.io',
-  vite: {
-    plugins: [tailwindcss()]
-  }
+  site: 'https://astronaut.github.io'
 })
-
