5df0bc58d64c33f0def187573a43188ccee5402c
Maxime Costalonga <maxime.costalonga.external@eviden.com>
2025-02-23 23:16:46 +0000

    feat: add copy button to markdown code blocks

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,36 @@
 // @ts-check
 import { defineConfig } from 'astro/config'
 
-// https://astro.build/config
+const addCopyButton = () => {
+  return {
+    name: 'shiki-transformer-copy-button',
+    pre(node) {
+      const button = h('button', {
+        class: 'copy',
+        "data-code": this.source,
+        onclick: `
+          navigator.clipboard.writeText(this.dataset.code);
+          this.classList.add('copied');
+          setTimeout(() => this.classList.remove('copied'), 2000);
+        `
+      }, [
+        h('span', { class: 'ready' }),
+        h('span', { class: 'success' })
+      ])
+
+      node.children.push(button)
+    }
+  }
+}
+
 export default defineConfig({
-  site: 'https://astronaut.github.io'
+  site: 'https://astronaut.github.io',
+  markdown: {
+    shikiConfig: {
+      theme: 'tokyo-night',
+      transformers: [
+        addCopyButton()
+      ]
+    }
+  }
 })
